da2c860e05673a607eb1d2f9208ace007a829945
mrizwan <rizwan@servup.work>
2018-06-10 03:52:48 +0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Http, Response, Headers } from '@angular/http';
+import 'rxjs/add/operator/map'
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  data: any;
+  constructor(private http: Http) {
+  }
+  onNameKeyUp(event:any){
+    this.getMyBlog(event.target.value);
+  }
+
+  getMyBlog(id:number){
+    return this.http.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+                .map((res: Response) => res.json())
+                 .subscribe(data => {
+                        this.data = data; 
+                        console.log(data);
+                });
+  }
 }
